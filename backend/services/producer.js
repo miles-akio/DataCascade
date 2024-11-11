f66b09c2d8026d5983b912f36b9ef5374265e257
Miles Shinmachi <78616843+miles-akio@users.noreply.github.com>
2024-11-10 22:00:50 -0800

    Update producer.js

--- backend/services/producer.js
+++ backend/services/producer.js
@@ -1,1 +1,28 @@
+// Backend directory contains services for:
+    // Kafka production and consumption
+    // DynamoDB - Model that is used to save data
+    // Server setup file for handling WebSocket connections & integrating Kafka
 
+// Producer.js Purpose:
+    // Kafka producer generates random data at intervals & sends it to a Kafka topic
+    // Simulaates real-time data that frontend will visualize
+
+/* -------------------------------------------------------------------*/
+
+// Import Kafka and create a producer instance
+const kafka = require('kafka-node');
+const { Producer } = kafka;
+
+// Kafka client connected to localhost broker
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = new Producer(client);
+
+// Producer Ready Event
+producer.on('ready', () => {
+    // Sends data every second
+    setInterval(() => {
+        const data = {
+            id: Date.now(), // Unique ID based on the timestamp
+            time: new Date().toISOString(), // Current time
+        }
+})
